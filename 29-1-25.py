38051b6ef3093cf17e2322254f666a20f1d31299
choutupellyabhishek <choutpelly.abhishek@gmail.com>
2025-02-23 18:17:09 +0530

    Add files via upload

---
+++ 29-1-25.py
@@ -0,0 +1,29 @@
+#and operator
+print(True and True)
+print(True and False)
+print(False and True)
+print(3 and 6)
+print(True and 'abhi')
+print ( 'abhi' and 3)
+print('' and 2)
+#or operator
+print(True or False)
+print(False or True)
+print(True or ((True and False) or (False or True) ))
+print(3 or 2)
+print(2 or 3)
+print(True or "")
+print(False or (True and True) )
+
+#not operator 
+print("----new------")
+print(not True)
+print(not 1)
+print(not 'str')
+
+# bitwise operators
+print( 2 & 4)
+print( 12 | 14)
+print( 2 << 1)
+print(2 >> 1)
+print(0b10100)
